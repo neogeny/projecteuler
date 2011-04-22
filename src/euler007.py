0321ed4b734d92834a982ea5197cc92453156da9
Juancarlo Añez <apalala@gmail.com>
2011-04-22 09:46:37 -0530

    First correct solution

---
+++ src/euler007.py
@@ -0,0 +1,31 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
+#we can see that the 6th prime is 13.
+#
+#What is the 10001st prime number?
+
+__known_primes = [2, 3, 5, 7,
+           11, 13, 17, 19,
+           23, 29,
+           31, 37,
+           41, 43, 47,
+           53, 59,
+           61, 67,
+           71, 73, 79,
+           83, 89,
+           97
+           ]
+def nth_prime(n):
+    if n <= len(__known_primes):
+        return __known_primes[n - 1]
+    n -= len(__known_primes)
+    k = __known_primes[-1] + 2
+    while n > 0:
+        for p in __known_primes:
+            if not k % p: break
+        else:
+            __known_primes.append(k)
+            n -= 1
+        k += 2
+    return __known_primes[-1]
+
+print nth_prime(10001)
