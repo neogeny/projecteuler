118addb6cfc7ff9bca856118fa0f3b57432e3cbb
Juancarlo Añez <apalala@gmail.com>
2011-05-04 16:58:45 -0530

    Refactorings plus solved problem 67: max cost of route from top to bottom in a large triangle.

--- src/euler018.py
+++ src/euler018.py
@@ -31,54 +31,20 @@ STRIANGLE = """
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 """
 
-from collections import namedtuple
 from copy import deepcopy
-from heapq import heappop as pop, heappush as push
-
-Node = namedtuple('Node', 'cost i j')
-Graph = namedtuple('Graph', 'nodes edges')
+from graphs import build_graph_from_triangle, find_max_path
 
 LINES = [s.split() for s in STRIANGLE.strip().split('\n')]
 TRIANGLE = [[int(s) for s in line] for line in LINES]
 
-def buildgraph():
-    T = deepcopy(TRIANGLE)
-    edges = {}
-    for i in xrange(len(T)):
-        for j, c in enumerate(T[i]):
-            n = Node(c, i, j)
-            edges[n] = []
-            T[i][j] = n
-    for i in xrange(len(T) - 1):
-        for j in xrange(len(T[i])):
-            n = T[i][j]
-            edges[n].append(T[i + 1][j])
-            edges[n].append(T[i + 1][j + 1])
-    stop = Node(0, -1, -1)
-    edges[stop] = []
-    for j in xrange(len(T[-1]) - 1):
-        edges[T[-1][j]].append(stop)
-    start = T[0][0]
-    return (Graph(edges.keys(), edges), start, stop)
-
-def findpath(graph, start, stop):
-    f = max(n.cost for n in graph.nodes)
-    heap = [(f - start.cost, start, [])]
-    while heap:
-        c, n, p = pop(heap)
-        if n == stop:
-            return (f + f * len(p) - c, list(reversed([(x.i, x.j) for x in p])))
-        for m in graph.edges[n]:
-            push(heap, (f - m.cost + c, m, [n] + p))
-
-def bruteforce():
-    T = deepcopy(TRIANGLE)
+def bruteforce(triangle):
+    T = deepcopy(triangle)
     for i in reversed(xrange(len(T) - 1)):
         for j, _ in enumerate(T[i]):
             T[i][j] += max(T[i + 1][j], T[i + 1][j + 1])
     return T[0][0]
 
 if __name__ == '__main__':
-    graph, start, stop = buildgraph()
-    print findpath(graph, start, stop)
-    print bruteforce()
+    graph, start, stop = build_graph_from_triangle(TRIANGLE)
+    print find_max_path(graph, start, stop)
+    print bruteforce(TRIANGLE)
