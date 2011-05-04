e3cd1188417f5c68f5fee858616b2b171e757a13
Juancarlo Añez <apalala@gmail.com>
2011-05-04 14:29:59 -0530

    more library refactorings

--- src/euler010.py
+++ src/euler010.py
@@ -3,14 +3,9 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 """
-from primality import nth_prime
-from itertools import count
+from primality import all_primes
 
 TARGET = 2 * 10 ** 6
-
-def all_primes():
-    for n in count(1):
-        yield nth_prime(n)
 
 def sum_primes(up_to):
     result = 0
