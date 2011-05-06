6bec6449937fca0a5c5d5e96b685c33947caad2c
Juancarlo Añez <apalala@gmail.com>
2011-05-05 21:57:30 -0530

    Problem 028: sum of number in diagonal of a spiral

---
+++ src/euler028.py
@@ -0,0 +1,31 @@
+# -*- encoding:utf-8 -*-i
+"""
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+"""
+from math import sqrt
+
+def spd(m):
+    diag_no = 1
+    diag_sum = 1
+    for r in range(1, 1+m // 2):
+        diff = 2*r
+        for i in range(4):
+            diag_no += diff
+            diag_sum+=diag_no
+#            print diag_sum,diag_no,diff,r
+    return diag_sum
+
+if __name__ == '__main__':
+    print spd(5)
+    print spd(7)
+    print spd(1001)
