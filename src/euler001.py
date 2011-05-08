87fb648daa52940dbd30b4762e881f22e8b1cf7e
Juancarlo A?ez <juancarlo.anez@gmail.com>
2011-05-08 15:31:05 -0530

    Added the RATIONALE for what I'm doing.

--- src/euler001.py
+++ src/euler001.py
@@ -1,4 +1,16 @@
-# The sum of the numbers less than 1000 that are divisible by 3 or 5.
+# -*- encoding:utf-8 -*-i
+
+# Project Euler Problem 1
+# http://projecteuler.net/
+#
+# by Apalala <apalala@gmail.com>
+# (cc) Attribution, Share Alike 
+
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 def sum_of_3_or_5_div():
     sum = 0
