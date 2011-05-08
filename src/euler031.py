75b053019deea5e2ec3e0fbd15f3072502561d8f
Juancarlo A?ez <juancarlo.anez@gmail.com>
2011-05-08 12:36:51 -0530

    Solved problem 31: Ways to make 200p using English coins.

---
+++ src/euler031.py
@@ -0,0 +1,36 @@
+# -*- encoding:utf-8 -*-i
+"""
+In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+
+    1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+
+It is possible to make £2 in the following way:
+
+    1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+
+How many different ways can £2 be made using any number of coins?
+"""
+
+def add_change(amount, denominations):
+    if not amount:
+        yield []
+    elif denominations:
+        denominations = list(sorted(denominations))
+        d = denominations[-1]
+        denominations = denominations[:-1]
+        for i in xrange(1 + amount // d):
+            for w in add_change(amount - i*d, denominations):
+                if w is not None:
+                    if i:
+                        yield [(i, d)] + w
+                    else:
+                        yield w
+        
+
+if __name__ == '__main__':
+    ways = 0
+    for w in add_change(200, [1,2,5,10,20,50,100,200]):
+        assert 200 == sum(n*d for n,d in w)
+        print w
+        ways += 1
+    print ways
