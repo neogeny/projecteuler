0dbbc49a4c0fc09cc27e7c7310fd5c6594f6848d
Juancarlo Añez <apalala@gmail.com>
2011-05-08 16:16:16 -0530

    Preparation to add boilerplate

--- src/euler001.py
+++ src/euler001.py
@@ -1,11 +1,3 @@
-# -*- encoding:utf-8 -*-i
-
-# Project Euler Problem 1
-# http://projecteuler.net/
-#
-# by Apalala <apalala@gmail.com>
-# (cc) Attribution, Share Alike 
-
 """
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
