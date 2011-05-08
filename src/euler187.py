a3a9f4bfe396b8da294687d44bc81d136f1d02d1
Juancarlo A?ez <juancarlo.anez@gmail.com>
2011-05-07 18:32:14 -0530

    Added text for problem 187.

--- src/euler187.py
+++ src/euler187.py
@@ -1,9 +1,14 @@
 # -*- encoding:utf-8 -*-i
 """
+A composite is a number containing at least two prime factors. For example, 15 = 3 × 5; 9 = 3 × 3; 12 = 2 × 2 × 3.
+
+There are ten composites below thirty containing precisely two, not necessarily distinct, prime factors: 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
+
+How many composite integers, n < 108, have precisely two, not necessarily distinct, prime factors?
 """
-import pyximport; pyximport.install()
+#import pyximport; pyximport.install()
 from timeit import timeit
-#import libeuler187 as lib
+import libeuler187 as lib #@UnusedImport
 
 istmt = """import libeuler187 as lib
 """
