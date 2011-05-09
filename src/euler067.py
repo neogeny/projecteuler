b36ab92459cac60540d9722cdeefdfc51edf060d
Juancarlo Añez <apalala@gmail.com>
2011-05-09 15:49:37 -0530

    Broken boilerplate.

--- src/euler067.py
+++ src/euler067.py
@@ -22,7 +22,8 @@ That is, 3 + 7 + 4 + 9 = 23.
 Find the maximum total from top to bottom in triangle.txt (right click and 'Save Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
 
 NOTE: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
-'''
+"""
+
 from euler018 import bruteforce
 
 STRIANGLE = open('../data/triangle.txt', 'r').read()
