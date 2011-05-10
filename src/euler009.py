ee70eeb0a192f7200c73bf68f235a115810e6d56
Juancarlo Añez <apalala@gmail.com>
2011-05-09 21:49:00 -0530

    Tests up to problem 9.

--- src/euler009.py
+++ src/euler009.py
@@ -22,25 +22,33 @@ Find the product abc.
 # a2 + b2 = c2
 # a + b + c = 1000
 
-import math
+from math import sqrt
+from functools import reduce
 
 def pythagorean_triplet_that_sums(s):
     def solve_b(a):
-        return int(1000 * (500 - a) / (1000 - a))
+        return s * (s // 2 - a) // (s - a)
 
     for a in xrange(1, s):
         b = solve_b(a)
         if b < a:
             break
 
-        c = math.sqrt(a ** 2 + b ** 2)
+        c = sqrt(a ** 2 + b ** 2)
         if c != int(c):
             continue
         c = int(c)
 
 #        print a, b, c, a + b + c
         if a + b + c == s:
-            return a * b * c
+            return (a , b , c)
+
+def prod(q):
+    return reduce(lambda x, y:x * y, q, 1)
+
+def test():
+    assert 3 * 4 * 5 == prod(pythagorean_triplet_that_sums(3 + 4 + 5))
 
 if __name__ == '__main__':
-    print pythagorean_triplet_that_sums(1000)
+    test()
+    print prod(pythagorean_triplet_that_sums(1000))
