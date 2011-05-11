18ed519be1bf01ba68d58438225129b022af6162
Juancarlo Añez <apalala@gmail.com>
2011-05-11 10:40:52 -0530

    Optimizations, refactorings for readability, ans tests.

--- src/euler024.py
+++ src/euler024.py
@@ -9,18 +9,28 @@ by Apalala <apalala@gmail.com>
 (cc) Attribution-ShareAlike 
 http://creativecommons.org/licenses/by-sa/3.0/
 
-A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+A permutation is an ordered arrangement of objects. For example, 3124 is one 
+possible permutation of the digits 1, 2, 3 and 4. If all of the permutations 
+are listed numerically or alphabetically, we call it lexicographic order. The 
+lexicographic permutations of 0, 1 and 2 are:
 
 012   021   102   120   201   210
 
-What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 
+5, 6, 7, 8 and 9?
 """
 from itertools import permutations
 
-TARGET = 10 ** 6
+def nth_permutation(n, digits):
+    perms = permutations(digits)
+    for _ in xrange(n - 1):
+        perms.next()
+    return ''.join(perms.next())
+
+
+def test():
+    assert '210' == nth_permutation(6, '012')
 
 if __name__ == '__main__':
-    perms = permutations('0123456789')
-    for _ in xrange(TARGET - 1):
-        perms.next()
-    print ''.join(perms.next())
+    test()
+    print nth_permutation(10 ** 6, '0123456789')
