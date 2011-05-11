40b32737d92ae2601e0e09faf5abfa85313a70f3
Juancarlo A?ez <juancarlo.anez@gmail.com>
2011-05-11 11:17:58 -0530

    Problem 32 unsolved yet.

---
+++ src/euler032.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+"""
+Solution to Project Euler Problem 32
+http://projecteuler.net/
+
+by Apalala <apalala@gmail.com>
+(cc) Attribution-ShareAlike 
+http://creativecommons.org/licenses/by-sa/3.0/
+
+We shall say that an n-digit number is pandigital if it makes use of all the 
+digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 
+through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing 
+multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity
+can be written as a 1 through 9 pandigital.
+HINT: Some products can be obtained in more than one way so be sure to only
+include it once in your sum.
+"""
+
+def test():
+    assert False
+
+if __name__ == '__main__':
+    test()
+    pass
