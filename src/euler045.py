7f1c3f4e8724bf43187aacb84130ae9bcb51b463
Juancarlo A?ez <juancarlo.anez@gmail.com>
2011-05-20 13:22:48 -0530

    Refactorings to accomodate solution to problem 44

--- src/euler045.py
+++ src/euler045.py
@@ -20,24 +20,7 @@ It can be verified that T285 = P165 = H143 = 40755.
 
 Find the next triangle number that is also pentagonal and hexagonal.
 """
-from math import sqrt
-from itertools import count
-
-def is_triangle(n):
-    c = (-1 + sqrt(1 + 8 * n)) / 2
-    return c == int(c)
-
-def is_pentagonal(n):
-    c = (1 + sqrt(1 + 3 * 8 * n)) / 6
-    return c == int(c)
-
-def is_exagonal(n):
-    c = (1 + sqrt(1 + 8 * n)) / 4
-    return c == int(c)
-
-def exagonals():
-    for n in count(1):
-        yield n * (2 * n - 1)
+from series import exagonals, is_exagonal, is_pentagonal, is_triangle
 
 def next_multiagonal(upfrom):
     for n in exagonals():
@@ -45,13 +28,6 @@ def next_multiagonal(upfrom):
             return n
 
 def test():
-    assert is_triangle(6)
-    assert is_triangle(10)
-    assert not is_triangle(5)
-    assert is_pentagonal(5)
-    assert is_pentagonal(12)
-    assert is_exagonal(15)
-    assert not is_exagonal(10)
     assert is_triangle(40755)
     assert is_pentagonal(40755)
     assert is_exagonal(40755)
--- src/series.py
+++ src/series.py
@@ -12,8 +12,10 @@ http://creativecommons.org/licenses/by-sa/3.0/
 Mthematical series.
 """
 from operator import mul
-from memoization import memoize
 import math
+from math import sqrt
+from itertools import count
+from memoization import memoize
 
 @memoize
 def factorial(n):
@@ -31,12 +33,50 @@ def fibs(n):
             break
         yield f
 
-
 def sum_of_squares(n):
     return (n * (n + 1) * (2 * n + 1)) // 6
 
 def square_of_sum(n):
     return (n ** 2 * (n + 1) ** 2) // 4
 
+def is_triangle(n):
+    c = (-1 + sqrt(1 + 8 * n)) / 2
+    return c == int(c)
+
+def triangles(upto=None):
+    for n in count(1):
+        if upto and n > upto:
+            break
+        yield n * (n + 1) / 2
+
+def is_pentagonal(n):
+    c = (1 + sqrt(1 + 3 * 8 * n)) / 6
+    return c == int(c)
+
+def pentagonals(upto=None):
+    for n in count(1):
+        if upto and n > upto:
+            break
+        yield n * (3 * n - 1) / 2
+
+def is_exagonal(n):
+    c = (1 + sqrt(1 + 8 * n)) / 4
+    return c == int(c)
+
+def exagonals(upto=None):
+    for n in count(1):
+        if upto and n > upto:
+            break
+        yield n * (2 * n - 1)
+
+def test():
+    assert is_triangle(6)
+    assert is_triangle(10)
+    assert not is_triangle(5)
+    assert is_pentagonal(5)
+    assert is_pentagonal(12)
+    assert is_exagonal(15)
+    assert not is_exagonal(10)
+
 if __name__ == '__main__':
-    print list(fibs(4))
+    test()
