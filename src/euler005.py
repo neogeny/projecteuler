314d41ec5cb53548b77c4b50685e75890b6a3d55
Juancarlo A?ez <juancarlo.anez@gmail.com>
2013-10-12 12:02:03 -0530

    PEP8

--- src/euler005.py
+++ src/euler005.py
@@ -6,7 +6,7 @@ Solution to Project Euler Problem 5
 http://projecteuler.net/
 
 by Apalala <apalala@gmail.com>
-(cc) Attribution-ShareAlike 
+(cc) Attribution-ShareAlike
 http://creativecommons.org/licenses/by-sa/3.0/
 
 2520 is the smallest number that can be divided by each of the numbers
@@ -18,11 +18,14 @@ from factorization import mcm
 
 TARGET = 20
 
+
 def range_mcm(m):
     return mcm(xrange(2, m + 1))
 
+
 def test():
     assert 2520 == range_mcm(10)
+
 
 if __name__ == '__main__':
     test()
