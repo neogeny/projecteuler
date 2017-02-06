bb8a2d3b5f906b3d295f07855ba71cc14a87afa5
apalala <apalala@gmail.com>
2017-02-06 16:08:27 -0400

    Update to Python 3

--- solutions/euler026.py
+++ solutions/euler026.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
-# -*- encoding:utf-8 -*-
-
 """
 Solution to Project Euler Problem 26
 http://projecteuler.net/
 
 by Apalala <apalala@gmail.com>
-(cc) Attribution-ShareAlike 
+(cc) Attribution-ShareAlike
 http://creativecommons.org/licenses/by-sa/3.0/
 
 
-A unit fraction contains 1 in the numerator. The decimal representation of the 
+A unit fraction contains 1 in the numerator. The decimal representation of the
 unit fractions with denominators 2 to 10 are given:
 
     1/2 =   0.5
@@ -23,13 +21,13 @@ unit fractions with denominators 2 to 10 are given:
     1/9 =   0.(1)
     1/10    =   0.1
 
-Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be 
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be
 seen that 1/7 has a 6-digit recurring cycle.
 
-Find the value of d < 1000 for which 1/d contains the longest recurring cycle 
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle
 in its decimal fraction part.
 """
-from primality import primes_upto
+
 
 def long_division_pattern(d):
     seen = {}
@@ -43,12 +41,15 @@ def long_division_pattern(d):
             return k - seen[r]
     return 0
 
+
 def find_longest_recurring(m):
-    return max((long_division_pattern(i), i) for i in primes_upto(m))
+    return max((long_division_pattern(i), i) for i in range(2, m))
+
 
 def test():
     assert 7 == find_longest_recurring(10)[1]
 
+
 if __name__ == '__main__':
     test()
-    print find_longest_recurring(1000)[1]
+    print(find_longest_recurring(1000)[1])
