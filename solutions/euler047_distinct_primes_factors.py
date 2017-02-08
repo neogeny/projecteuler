145d598d9e07978448cdd2e9d7a862ed9cc79c23
Juancarlo Añez <apalala@gmail.com>
2017-02-08 01:40:39 -0400

    Update to Python 3 and optimize

--- solutions/euler047_distinct_primes_factors.py
+++ solutions/euler047_distinct_primes_factors.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-# -*- encoding:utf-8 -*-
-
 """
 Solution to Project Euler Problem 47
 http://projecteuler.net/
 
 by Apalala <apalala@gmail.com>
-(cc) Attribution-ShareAlike 
+(cc) Attribution-ShareAlike
 http://creativecommons.org/licenses/by-sa/3.0/
 
+Distinct primes factors
 
 The first two consecutive numbers to have two distinct prime factors are:
 
@@ -21,36 +20,41 @@ The first three consecutive numbers to have three distinct prime factors are:
 645 = 3 × 5 × 43
 646 = 2 × 17 × 19.
 
-Find the first four consecutive integers to have four distinct primes factors. 
+Find the first four consecutive integers to have four distinct primes factors.
 What is the first of these numbers?
 """
-from itertools import count
 from factorization import factors
+
 
 def n_factors(n):
     return len(list(factors(n)))
 
-def k_consecutive_with_k_factors(k):
-    for n in count(5):
-        for i in xrange(n, n + k):
-            if n_factors(i) != k:
+
+def first_k_consecutive_with_k_factors(k):
+    n = k
+    while True:
+        for i in range(k):
+            if n_factors(n + i) != k:
+                n += i + 1
                 break
         else:
             return n
 
+
 def test():
     assert 2 == n_factors(14)
     assert 2 == n_factors(15)
-    assert 3 == n_factors(644)
+    assert 3 == n_factors(644), str(n_factors(644)) + ' - ' + str(factors(644))
     assert 3 == n_factors(645)
     assert 3 == n_factors(646)
-    assert 14 == k_consecutive_with_k_factors(2)
-    assert 644 == k_consecutive_with_k_factors(3)
+    assert 14 == first_k_consecutive_with_k_factors(2)
+    assert 644 == first_k_consecutive_with_k_factors(3)
+
+
+def run():
+    print(first_k_consecutive_with_k_factors(4))
+
 
 if __name__ == '__main__':
     test()
-    n = k_consecutive_with_k_factors(4)
-    for i in xrange(n, n + 4):
-        print i, list(factors(i))
-
-
+    run()
