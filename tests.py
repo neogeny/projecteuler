bdc00f4a16ef2f922ea345a9bef8e470a63e06e9
apalala <apalala@gmail.com>
2017-02-07 21:18:56 -0400

    Refactoring of script to run tests

--- tests.py
+++ tests.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-# -*- encoding:utf-8 -*-
-
 """
 Solutions to Project Euler Problems
 http://projecteuler.net/
@@ -8,13 +6,11 @@ http://projecteuler.net/
 by Apalala <apalala@gmail.com>
 (cc) Attribution-ShareAlike
 http://creativecommons.org/licenses/by-sa/3.0/
-
-
 """
-import sys
-from glob import glob
-import os
+from pathlib import Path
 from timeit import timeit
+
+import sys
 sys.path.insert(0, 'solutions')
 
 
@@ -27,19 +23,26 @@ def test_assertions_on():
         pass
 
 
-if __name__ == '__main__':
+def timed_test(name):
+    return timeit(
+        'test();run()',
+        'from ' + name + ' import test, run',
+        number=1
+    )
+
+
+def main():
     test_assertions_on()
-    total = 0
-    count = 0
+
+    total_time = 0
+    success_count = 0
     failed = 0
-    for filename in sorted(glob('solutions/euler*.py')):
-        filename = os.path.basename(filename)
-        name, _ = os.path.splitext(filename)
+    modules = [p.stem for p in Path().glob('solutions/euler*.py')]
+    for name in modules:
         print('%-40s ' % name, end='')
         try:
-            t = timeit('test();run()', 'from ' + name + ' import test, run', number=1)
-            total += t
-            count += 1
+            total_time += timed_test(name)
+            success_count += 1
         except KeyboardInterrupt:
             break
         except ImportError as e:
@@ -50,5 +53,10 @@ if __name__ == '__main__':
         except Exception as e:
             failed += 1
             print(e)
+
     print()
-    print('total time for', count, 'problems,', failed, 'failed, is:', total)
+    print('total time for', success_count, 'problems,', failed, 'failed, is:', total_time)
+
+
+if __name__ == '__main__':
+    main()
