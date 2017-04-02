3980a5f39d211aac7be1ad2677cc8f427d1020e9
Apalala <apalala@gmail.com>
2017-04-02 08:38:12 -0400

    Changed 001 to be O(1) and thus meet timing requirements of HackerRank

--- solutions/euler001.py
+++ solutions/euler001.py
@@ -13,17 +13,26 @@ of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+def sum_of_multiples(n, limit):
+    k = (limit - 1) // n
+    return ((n + k * n) * k) // 2
 
-def sum_of_3_or_5_div(m):
-    return sum(i for i in range(m) if not (i % 3 and i % 5))
+
+def sum_multiples_of_3or5(limit):
+    return (
+        sum_of_multiples(3, limit) +
+        sum_of_multiples(5, limit) -
+        sum_of_multiples(15, limit)
+    )
+
 
 
 def test():
-    assert 23 == sum_of_3_or_5_div(10)
+    assert 23 == sum_multiples_of_3or5(10)
 
 
 def run():
-    print(sum_of_3_or_5_div(1000))
+    print(sum_multiples_of_3or5(1000))
 
 
 if __name__ == '__main__':
